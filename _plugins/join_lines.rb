1515b7476af9a00ae21f3c07c677ba95b495670b
Roland Haas <rhaas@illinois.edu>
2021-04-18 20:32:39 -0500

    plugins: remove use of join_lines plugin

--- _plugins/join_lines.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module JoinLinesFilter
-    def join_lines(input)
-      input.to_s.gsub(/\n */, "")
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::JoinLinesFilter)
